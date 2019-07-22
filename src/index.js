3971009de06e7cf671bf656837809128e8bc6776
David Huang <david.huang@getac.com.tw>
2019-07-22 14:03:35 +0800

    Send mock request to server

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './component/App';
 import Welcome from './component/Welcome';
 import Signup from './component/auth/Signup';
 import reducers from './reducers';
 
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Provider store={store}>
     <BrowserRouter>
       <App>
         <Route path="/" exact component={Welcome} />
